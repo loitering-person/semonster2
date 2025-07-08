fee349e83636a8f6c6896757058837a3c6233a1c
loitering-person <vannesstjie@gmail.com>
2025-07-09 03:26:00 +0900

    added GenDeckArr and Arr to LL converter (PlayerTest.java)

--- app/src/test/java/org/example/PlayerTest.java
+++ app/src/test/java/org/example/PlayerTest.java
@@ -4,9 +4,22 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 import java.util.LinkedList;
+
 //String monsters[] = { "スライム", "サハギン", "ドラゴン", "デュラハン", "シーサーペント" };
 
 public class PlayerTest {
+  int[] generatedDeckArr = { 3, 0, 2, 4, 3, 0, 3, 3, 0, 1 };
+
+  // テストのやり方：generatedDeckArrの内容を編集し、以下の関数でそれをlinked list（generated deck)にコピー
+  // 得られたlinked listは、playerインスタンスのコンストラクタで使う
+  public LinkedList<Integer> insertArrToLL(LinkedList<Integer> inputLL) {
+    LinkedList<Integer> outputLL = new LinkedList<>();
+    for (int i : this.generatedDeckArr) {
+      outputLL.add(i);
+    }
+    return outputLL;
+  }
+
   @Test
   public void testdrawMonsters() {
     LinkedList<Integer> generatedDeck = new LinkedList<Integer>();
