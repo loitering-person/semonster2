5565e457f51b48e36327c02e077507b89ee4a711
loitering-person <vannesstjie@gmail.com>
2025-07-09 03:16:06 +0900

    adjusted tests to new constructor with empty LL (PlayerTest.java)

--- app/src/test/java/org/example/PlayerTest.java
+++ app/src/test/java/org/example/PlayerTest.java
@@ -3,17 +3,24 @@ package org.example;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+import java.util.LinkedList;
+//String monsters[] = { "スライム", "サハギン", "ドラゴン", "デュラハン", "シーサーペント" };
+
 public class PlayerTest {
   @Test
   public void testdrawMonsters() {
-    Player player = new Player("test");
+    LinkedList<Integer> generatedDeck = new LinkedList<Integer>();
+
+    Player player = new Player("test", generatedDeck);
 
     assertEquals(8, player.deck.size());
   }
 
   @Test
   public void testtoString() {
-    Player player = new Player("test");
+    LinkedList<Integer> generatedDeck = new LinkedList<Integer>();
+
+    Player player = new Player("test", generatedDeck);
 
     assertNotNull("Check Deck", player.toString());
   }
