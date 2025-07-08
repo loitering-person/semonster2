cec519aee9faaf2763a86a981ea7bfb64fcf5826
loitering-person <vannesstjie@gmail.com>
2025-07-09 03:10:31 +0900

    adapted tests to new constructor (MonsterTest.java)

--- app/src/test/java/org/example/MonsterTest.java
+++ app/src/test/java/org/example/MonsterTest.java
@@ -3,23 +3,25 @@ package org.example;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+//String monsters[] = { "スライム", "サハギン", "ドラゴン", "デュラハン", "シーサーペント" };
+
 public class MonsterTest {
   @Test
   public void testMonsterCreation() {
-    Monster monster = new Monster("スライム", 1);
+    Monster monster = new Monster(0, 1);
     assertEquals("スライム:レア度[1]", monster.toString());
   }
 
   @Test
   public void testSummonMonster() {
-    Monster monster = new Monster("サハギン", 2);
+    Monster monster = new Monster(1, 2);
     String summoned = monster.summonMonster(3);
     assertEquals("デュラハン", summoned);
   }
 
   @Test
   public void testEvolve() {
-    Monster monster = new Monster("ドラゴン", 3);
+    Monster monster = new Monster(2, 3);
     monster.evolve();
     assertEquals("ドラゴンナイト:レア度[3]", monster.toString());
   }
